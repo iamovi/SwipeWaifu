9e08b402ad93cec5c72f79aceb1b0c50a8e52b6b
Ovi ren <oviren.human@gmail.com>
2026-02-17 22:14:06 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -48,6 +48,7 @@ export default defineConfig(({ mode }) => ({
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+        navigateFallbackDenylist: [/^\/SwipeWaifu\/docs/],
       },
     }),
   ].filter(Boolean),
