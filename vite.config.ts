8e87733c9941609d9368a5c7dd5b0cd9ce756ca9
Ovi ren <oviren.human@gmail.com>
2026-02-17 13:38:21 +0600

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/waifu-gallery/',
+  base: '/SwipeWaifu/',
   server: {
     host: "::",
     port: 8080,
